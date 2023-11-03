d22c0a669c5661d8b3acde2d1774846ffea67523
Fauzan Akmal Mahdi <recovery252@gmail.com>
2023-11-04 01:36:18 +0700

    implement bloc to login  page

--- lib/presentation/page/login_page/login_page.dart
+++ lib/presentation/page/login_page/login_page.dart
@@ -1,9 +1,15 @@
+import 'package:fam_flutter_storyapp/data/model/request/login_request_model.dart';
+import 'package:fam_flutter_storyapp/data/repository/remote/account_repository.dart';
+import 'package:fam_flutter_storyapp/presentation/page/login_page/bloc/login_bloc.dart';
 import 'package:fam_flutter_storyapp/presentation/page/main_page.dart';
 import 'package:fam_flutter_storyapp/presentation/page/register_page/register_page.dart';
 import 'package:fam_flutter_storyapp/presentation/widget/app_mainbutton_widget.dart';
+import 'package:fam_flutter_storyapp/presentation/widget/app_screen_loading_widget.dart';
 import 'package:fam_flutter_storyapp/presentation/widget/app_textfield_widget.dart';
 import 'package:fam_flutter_storyapp/support/app_color.dart';
+import 'package:fam_flutter_storyapp/support/app_dialog_action.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -27,94 +33,142 @@ class _LoginPageState extends State<LoginPage> {
         FocusManager.instance.primaryFocus?.unfocus();
       },
       child: Scaffold(
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: 16.h,
-              vertical: 16.h,
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(height: 130.h),
-                Text(
-                  "Login Page",
-                  style: GoogleFonts.inter(
-                    color: Colors.black,
-                    fontSize: 28.sp,
-                    fontWeight: FontWeight.w500,
-                  ),
+        body: Stack(
+          children: [
+            SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 16.h,
+                  vertical: 16.h,
                 ),
-                SizedBox(height: 40.h),
-                AppTextFieldWidget(
-                  textFieldTitle: "Email / Username",
-                  textFieldHintText: "Email / Username",
-                  controller: emailTextFieldController,
-                ),
-                SizedBox(height: 10.h),
-                AppTextFieldWidget(
-                  textFieldTitle: "Password",
-                  textFieldHintText: "Password",
-                  controller: passwordTextFieldController,
-                  obscureText: showPassword,
-                  suffixIcon: IconButton(
-                    onPressed: () {
-                      setState(() {
-                        showPassword = !showPassword;
-                      });
-                    },
-                    icon: Icon(
-                      Icons.remove_red_eye,
-                      color: showPassword ? AppColor.disabled : AppColor.primary,
-                    ),
-                  ),
-                ),
-                SizedBox(height: 20.h),
-                AppMainButtonWidget(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) {
-                          return const MainPage();
-                        },
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(height: 130.h),
+                    Text(
+                      "Login Page",
+                      style: GoogleFonts.inter(
+                        color: Colors.black,
+                        fontSize: 28.sp,
+                        fontWeight: FontWeight.w500,
                       ),
-                    );
-                  },
-                  text: 'Login',
-                ),
-                SizedBox(height: 20.h),
-                Center(
-                  child: Text(
-                    "Belum Ada Akun?",
-                    style: GoogleFonts.inter(
-                      color: Colors.black,
-                      fontSize: 14.sp,
-                      fontWeight: FontWeight.w500,
                     ),
-                  ),
-                ),
-                SizedBox(height: 20.h),
-                AppMainButtonWidget(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) {
-                          return const RegisterPage();
+                    SizedBox(height: 40.h),
+                    AppTextFieldWidget(
+                      textFieldTitle: "Email / Username",
+                      textFieldHintText: "Email / Username",
+                      controller: emailTextFieldController,
+                    ),
+                    SizedBox(height: 10.h),
+                    AppTextFieldWidget(
+                      textFieldTitle: "Password",
+                      textFieldHintText: "Password",
+                      controller: passwordTextFieldController,
+                      obscureText: showPassword,
+                      suffixIcon: IconButton(
+                        onPressed: () {
+                          setState(() {
+                            showPassword = !showPassword;
+                          });
                         },
+                        icon: Icon(
+                          Icons.remove_red_eye,
+                          color: showPassword ? AppColor.disabled : AppColor.primary,
+                        ),
                       ),
-                    );
-                  },
-                  text: "Register",
+                    ),
+                    SizedBox(height: 20.h),
+                    AppMainButtonWidget(
+                      onPressed: () {
+                        if (emailTextFieldController.text.isEmpty || passwordTextFieldController.text.isEmpty) {
+                          AppDialogAction.showFailedPopup(
+                            context: context,
+                            title: "Something's wrong",
+                            description: "There's still empty field",
+                            buttonTitle: 'Back',
+                          );
+                        } else {
+                          context.read<LoginBloc>().add(
+                                ActionLogin(
+                                  loginRequestModel: LoginRequestModel(
+                                    email: emailTextFieldController.text,
+                                    password: passwordTextFieldController.text,
+                                  ),
+                                  accountRepository: AccountRepository(),
+                                ),
+                              );
+                        }
+                      },
+                      text: 'Login',
+                    ),
+                    SizedBox(height: 20.h),
+                    Center(
+                      child: Text(
+                        "Belum Ada Akun?",
+                        style: GoogleFonts.inter(
+                          color: Colors.black,
+                          fontSize: 14.sp,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20.h),
+                    AppMainButtonWidget(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return const RegisterPage();
+                            },
+                          ),
+                        );
+                      },
+                      text: "Register",
+                    ),
+                    SizedBox(height: 20.h),
+                  ],
                 ),
-                SizedBox(height: 20.h),
-              ],
+              ),
             ),
-          ),
+            loadingState(),
+            // const AppScreenLoadingWidget(),
+          ],
         ),
       ),
+    );
+  }
+
+  Widget loadingState() {
+    return BlocConsumer<LoginBloc, LoginState>(
+      builder: (context, state) {
+        if (state is LoginLoading) {
+          return const AppScreenLoadingWidget();
+        } else {
+          return const SizedBox();
+        }
+      },
+      listener: (context, state) {
+        if (state is LoginFailed) {
+          AppDialogAction.showFailedPopup(
+            context: context,
+            title: "Something's wrong",
+            description: state.errorMessage,
+            buttonTitle: "Back",
+          );
+        }
+        if (state is LoginSuccess) {
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+              builder: (context) {
+                return const MainPage();
+              },
+            ),
+          );
+        }
+      },
     );
   }
 }
