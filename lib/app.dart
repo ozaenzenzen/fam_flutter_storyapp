d36f620aed8542580e4e04a2281ed6e50f49a3b1
Fauzan Akmal Mahdi <recovery252@gmail.com>
2023-11-03 22:42:34 +0700

    setup screenutil

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:fam_flutter_storyapp/presentation/page/main_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
@@ -11,13 +12,21 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'FAM - Story App',
-      theme: ThemeData(
-        // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MainPage(),
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'FAM - Story App',
+          theme: ThemeData(
+            // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+            useMaterial3: true,
+          ),
+          home: const MainPage(),
+        );
+      },
     );
   }
 }
