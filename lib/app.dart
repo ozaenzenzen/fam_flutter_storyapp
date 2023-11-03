07f00f6fd9a1d07ae04ab4249b7e767d429efa6f
Fauzan Akmal Mahdi <recovery252@gmail.com>
2023-11-04 00:08:12 +0700

    setup app & main config

--- lib/app.dart
+++ lib/app.dart
@@ -1,3 +1,5 @@
+import 'package:fam_flutter_storyapp/data/repository/local/local_repository.dart';
+import 'package:fam_flutter_storyapp/presentation/page/login_page/login_page.dart';
 import 'package:fam_flutter_storyapp/presentation/page/main_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -10,10 +12,20 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  bool isLogin = false;
+
+  @override
+  void didChangeDependencies() async {
+    super.didChangeDependencies();
+    isLogin = await LocalRepository.getIsLogin();
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-      designSize: const Size(360, 690),
+      // designSize: const Size(360, 690),
+      designSize: const Size(411, 869),
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (context, child) {
@@ -24,7 +36,8 @@ class _MyAppState extends State<MyApp> {
             // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
             useMaterial3: true,
           ),
-          home: const MainPage(),
+          // home: const MainPage(),
+          home: isLogin ? const MainPage() : const LoginPage(),
         );
       },
     );
--- lib/init_config.dart
+++ lib/init_config.dart
@@ -1,5 +1,11 @@
+import 'package:fam_flutter_storyapp/env.dart';
+import 'package:fam_flutter_storyapp/support/app_connectivity_service.dart';
+import 'package:fam_flutter_storyapp/support/app_info.dart';
+
 class InitConfig {
-  static void initialization() {
-    //
+  static Future<void> initialization() async {
+    AppInfo.appInfoInit();
+    AppConnectivityService.init();
+    EnvironmentConfig.customBaseUrl = "https://story-api.dicoding.dev/v1";
   }
 }
