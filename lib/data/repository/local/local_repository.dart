74dd1e90c815be59c37438f4536b72c50b3aca9a
Fauzan Akmal Mahdi <recovery252@gmail.com>
2023-11-04 00:29:36 +0700

    update local repo: set get userdata

--- lib/data/repository/local/local_repository.dart
+++ lib/data/repository/local/local_repository.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:fam_flutter_storyapp/domain/entities/user_data_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_keychain/flutter_keychain.dart';
 
@@ -23,6 +24,28 @@ class LocalRepository {
       await FlutterKeychain.put(key: 'isLogin', value: jsonEncode(value));
     } catch (e) {
       debugPrint('error setLogin $e');
+    }
+  }
+
+  static Future<UserDataModel?> getUserData(String value) async {
+    try {
+      String isLogin = await FlutterKeychain.get(key: 'userData') ?? "";
+      if (isLogin == "") {
+        return null;
+      } else {
+        UserDataModel decode = UserDataModel.fromJson(jsonDecode(isLogin));
+        return decode;
+      }
+    } catch (e) {
+      return null;
+    }
+  }
+
+  static Future<void> setUserData(UserDataModel value) async {
+    try {
+      await FlutterKeychain.put(key: 'userData', value: jsonEncode(value.toJson()));
+    } catch (e) {
+      debugPrint('error setUserData $e');
     }
   }
 }
