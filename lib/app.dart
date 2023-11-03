e6339ddbb539ca96282ad36ee95fce4024498982
Fauzan Akmal Mahdi <recovery252@gmail.com>
2023-11-03 22:24:09 +0700

    setup config app

---
+++ lib/app.dart
@@ -0,0 +1,23 @@
+import 'package:fam_flutter_storyapp/presentation/main_page.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FAM - Story App',
+      theme: ThemeData(
+        // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MainPage(),
+    );
+  }
+}
---
+++ lib/init_config.dart
@@ -0,0 +1,5 @@
+class InitConfig {
+  static void initialization() {
+    //
+  }
+}
