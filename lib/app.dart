634465a8d4947ea8d383657e20ca4a2dd0691fbf
Fauzan Akmal Mahdi <recovery252@gmail.com>
2023-11-03 22:29:28 +0700

    update routes

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,4 @@
-import 'package:fam_flutter_storyapp/presentation/main_page.dart';
+import 'package:fam_flutter_storyapp/presentation/page/main_page.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatefulWidget {
