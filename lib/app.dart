ea8ea7a4d12ddfeae6ab364f2bc82414c34c5835
Fauzan Akmal Mahdi <recovery252@gmail.com>
2023-11-06 11:17:57 +0700

    set showpassword bloc

--- lib/app.dart
+++ lib/app.dart
@@ -1,6 +1,7 @@
 import 'package:fam_flutter_storyapp/presentation/page/login_page/check_islogin_bloc/check_islogin_bloc.dart';
 import 'package:fam_flutter_storyapp/presentation/page/login_page/login_bloc/login_bloc.dart';
 import 'package:fam_flutter_storyapp/presentation/page/login_page/login_page.dart';
+import 'package:fam_flutter_storyapp/presentation/page/login_page/show_password_bloc/show_password_bloc.dart';
 import 'package:fam_flutter_storyapp/presentation/page/logout_page/bloc/logout_bloc.dart';
 import 'package:fam_flutter_storyapp/presentation/page/main_page.dart';
 import 'package:fam_flutter_storyapp/presentation/page/register_page/bloc/register_bloc.dart';
@@ -36,6 +37,9 @@ class _MyAppState extends State<MyApp> {
         ),
         BlocProvider(
           create: (context) => CheckIsloginBloc(),
+        ),
+        BlocProvider(
+          create: (context) => ShowPasswordBloc(),
         ),
       ],
       child: ScreenUtilInit(
