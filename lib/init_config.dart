462eeb5a4a348d748436f03edd5bd5e7f56a7f2a
Fauzan Akmal Mahdi <recovery252@gmail.com>
2023-11-10 17:59:50 +0700

    update init config with permission location

--- lib/init_config.dart
+++ lib/init_config.dart
@@ -1,11 +1,13 @@
 import 'package:fam_flutter_storyapp/env.dart';
 import 'package:fam_flutter_storyapp/support/app_connectivity_service.dart';
 import 'package:fam_flutter_storyapp/support/app_info.dart';
+import 'package:fam_flutter_storyapp/support/app_location_service.dart';
 
 class InitConfig {
   static Future<void> initialization() async {
     AppInfo.appInfoInit();
     AppConnectivityService.init();
     EnvironmentConfig.customBaseUrl = "https://story-api.dicoding.dev/v1";
+    await AppLocationService.permissionHandlerLocation();
   }
 }
