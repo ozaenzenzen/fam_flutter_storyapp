17ff393c613aa3403dab47768f8db47a63396c38
Fauzan Akmal Mahdi <recovery252@gmail.com>
2023-11-11 07:54:37 +0700

    set flavor

---
+++ lib/main_development.dart
@@ -0,0 +1,12 @@
+import 'package:fam_flutter_storyapp/env.dart';
+import 'package:fam_flutter_storyapp/init_config.dart';
+import 'package:flutter/material.dart';
+
+import 'app.dart';
+
+void main() async {
+  EnvironmentConfig.flavor = Flavor.development;
+  WidgetsFlutterBinding.ensureInitialized();
+  await InitConfig.initialization();
+  runApp(const MyApp());
+}
